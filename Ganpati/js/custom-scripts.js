ef920accd6759f19ebd6495fe449c89bb917bca4
Mohammad Javed <javedmohammad485@gmail.com>
2024-05-12 03:40:14 +0530

    Upload

--- Ganpati/js/custom-scripts.js
+++ Ganpati/js/custom-scripts.js
@@ -15,9 +15,7 @@ jQuery(function ($) {
         rtl_status = true;
     }
 
-
     // DETECT TOUCH DEVICE
-
     function is_touch_device() {
         return !!('ontouchstart' in window) || (!!('onmsgesturechange' in window) && !!window.navigator.maxTouchPoints);
     }
@@ -44,46 +42,6 @@ jQuery(function ($) {
                 verticalOffset: 50
             });
         }
-    }
-
-    // BANNER.
-    if ($(".section-banner").length) {
-        $(".section-banner").each(function () {
-            var $this = $(this);
-            var bg_img = "images/home_1_slider_1.jpg",
-                bg_color = "#000000",
-                bg_opacity = "0.5",
-                bg_color_2 = "#000000",
-                bg_opacity_2 = "0.8";
-            if ($this.is('[data-bg_img]')) {
-                bg_img = ', url("' + $this.data('bg_img') + '")';
-            } else {
-                bg_img = ', url("' + bg_img + '")';
-            }
-
-            if ($this.is('[data-bg_color]')) {
-                bg_color = $this.data('bg_color');
-            }
-
-            if ($this.is('[data-bg_opacity]')) {
-                bg_opacity = $this.data('bg_opacity');
-            }
-
-            var $color_overlay = hexToRgbA(bg_color, bg_opacity);
-            $color_overlay_2 = $color_overlay;
-            if ($this.is('[data-gardient]') && $this.data('gardient') === true) {
-                if ($this.is('[data-bg_color_2]')) {
-                    bg_color_2 = $this.data('bg_color_2');
-                }
-
-                if ($this.is('[data-bg_opacity_2]')) {
-                    bg_opacity_2 = $this.data('bg_opacity_2');
-                }
-
-                var $color_overlay_2 = hexToRgbA(bg_color_2, bg_opacity_2);
-            }
-            $this.attr("style", "background:linear-gradient( " + $color_overlay + ",  " + $color_overlay_2 + " )" + bg_img + "; background-position: center center; background-repeat: repeat; background-attachment: inherit; background-size: cover; overflow:hidden;");
-        });
     }
 
     // SLIDER 1
@@ -159,7 +117,6 @@ jQuery(function ($) {
             $this.find('.item').before('<div class="slide-bg"></div>');
 
             $this.find('.slide-bg').attr("style", "background:linear-gradient( " + $color_overlay + ",  " + $color_overlay + " )" + bg_img + "; background-position: center center; background-repeat: no-repeat; background-attachment: inherit; background-size: cover; overflow:hidden;");
-
         });
 
         slider_resize();
@@ -267,13 +224,6 @@ jQuery(function ($) {
     }
 
     // STICKY HEADER & MENU
-    if ($(".main-header").attr('data-sticky_header')) {
-        $('.main-header .header-wrapper').waypoint('sticky', {
-            wrapper: '<div class="sticky-wrapper" />',
-            stuckClass: 'stuck'
-        });
-    }
-
     if ($(".main-header-2").length) {
         if ($(".main-header-2").attr('data-sticky_header')) {
             $('.main-header-2 .navbar-default').waypoint('sticky', {
@@ -372,148 +322,7 @@ jQuery(function ($) {
         });
     }
 
-
-    // HIGHLIGHT CAROUSEL
-    if ($(".highlight-carousel").length) {
-        var $highlight_carousel = $('.highlight-carousel');
-        $highlight_carousel.each(function () {
-            var $this = $(this);
-            var items_val = 3,
-                nav_val = true,
-                dots_val = true,
-                autoplay_val = true,
-                autoplaytimeout_val = 5000;
-            // Status.
-            if ($this.attr('data-carousel') && $this.data('carousel') !== 1) {
-                $this.removeClass('owl-carousel');
-                return '';
-            }
-            // no of items
-            if ($this.attr('data-items') && !isNaN($this.data('items'))) {
-                items_val = $this.data('items');
-            }
-            // navigation status.
-            if ($this.attr('data-nav') && !isNaN($this.data('nav'))) {
-                nav_val = $this.data('nav');
-            }
-
-            // navigation status.
-            if ($this.attr('data-dots') && !isNaN($this.data('dots'))) {
-                dots_val = $this.data('dots');
-            }
-            // Autoplay status.
-            if ($this.attr('data-autoplay') && !isNaN($this.data('autoplay'))) {
-                autoplay_val = $this.data('autoplay');
-            }
-            // Autoplay status.
-            if ($this.attr('data-autoplaytimeout') && !isNaN($this.data('autoplaytimeout'))) {
-                autoplaytimeout_val = $this.data('autoplaytimeout');
-            }
-
-            $this.owlCarousel({
-                rtl: rtl_status,
-                items: items_val,
-                loop: true,
-                autoplay: autoplay_val,
-                autoplayTimeout: autoplaytimeout_val,
-                autoplayHoverPause: true,
-                dots: dots_val,
-                nav: nav_val,
-                navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: false,
-                        dots: false
-                    },
-                    720: {
-                        items: 2,
-                        nav: false
-                    },
-                    1000: {
-                        items: items_val
-                    }
-                }
-            });
-        });
-    }
-
-    // SERVICE.
-
-    if ($(".service-carousel").length) {
-        var $service_carousel = $('.service-carousel');
-        $service_carousel.each(function () {
-            var $this = $(this);
-
-            var items_val = 3,
-                sm_items_val = 1,
-                nav_val = false,
-                dots_val = false,
-                autoplay_val = false,
-                autoplaytimeout_val = 5000;
-            // Status.
-            if ($this.attr('data-carousel') && $this.data('carousel') !== 1) {
-
-                $this.removeClass('owl-carousel');
-                return '';
-            }
-            // no of items
-            if ($this.attr('data-items') && !isNaN($this.data('items'))) {
-                items_val = $this.data('items');
-            }
-
-            // no of items in small screen.
-            if ($this.attr('data-sm-items') && !isNaN($this.data('sm-items'))) {
-                sm_items_val = $this.data('sm-items');
-            }
-
-            // navigation status.
-            if ($this.attr('data-nav') && !isNaN($this.data('nav'))) {
-                nav_val = $this.data('nav');
-            }
-
-            // navigation status.
-            if ($this.attr('data-dots') && !isNaN($this.data('dots'))) {
-                dots_val = $this.data('dots');
-            }
-            // Autoplay status.
-            if ($this.attr('data-autoplay') && !isNaN($this.data('autoplay'))) {
-                autoplay_val = $this.data('autoplay');
-            }
-            // Autoplay status.
-            if ($this.attr('data-autoplaytimeout') && !isNaN($this.data('autoplaytimeout'))) {
-                autoplaytimeout_val = $this.data('autoplaytimeout');
-            }
-
-            $this.owlCarousel({
-                rtl: rtl_status,
-                items: items_val,
-                loop: true,
-                autoplay: autoplay_val,
-                autoplayTimeout: autoplaytimeout_val,
-                autoplayHoverPause: true,
-                dots: dots_val,
-                nav: nav_val,
-                navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-                responsive: {
-                    0: {
-                        items: sm_items_val,
-                        nav: false
-                    },
-                    600: {
-                        items: 2,
-                        nav: false
-                    },
-                    1000: {
-                        items: items_val
-                    }
-                }
-            });
-        });
-    }
-
     // GALLERY.
-
     if ($('.gallery-light-box').length) {
         $('.gallery-light-box').venobox();
     }
@@ -748,144 +557,6 @@ jQuery(function ($) {
         });
     }
 
-    // TEAM CAROUSEL.
-
-    if ($(".team-carousel").length) {
-        var $team_carousel = $('.team-carousel');
-        $team_carousel.each(function () {
-            var $this = $(this);
-            var items_val = 3,
-                nav_val = false,
-                dots_val = true,
-                autoplay_val = true,
-                autoplaytimeout_val = 5000;
-
-            // Status.
-            if ($this.attr('data-carousel') && $this.data('carousel') !== 1) {
-                $this.removeClass('owl-carousel');
-                return '';
-            }
-
-            // no of items
-            if ($this.attr('data-items') && !isNaN($this.data('items'))) {
-                items_val = $this.data('items');
-            }
-
-            // navigation status.
-            if ($this.attr('data-nav') && !isNaN($this.data('nav'))) {
-                nav_val = $this.data('nav');
-            }
-
-            // navigation status.
-            if ($this.attr('data-dots') && !isNaN($this.data('dots'))) {
-                dots_val = $this.data('dots');
-            }
-
-            // Autoplay status.
-            if ($this.attr('data-autoplay') && !isNaN($this.data('autoplay'))) {
-                autoplay_val = $this.data('autoplay');
-            }
-
-            // Autoplay status.
-            if ($this.attr('data-autoplaytimeout') && !isNaN($this.data('autoplaytimeout'))) {
-                autoplaytimeout_val = $this.data('autoplaytimeout');
-            }
-
-            $this.owlCarousel({
-                rtl: rtl_status,
-                items: items_val,
-                loop: true,
-                autoplay: autoplay_val,
-                autoplayTimeout: autoplaytimeout_val,
-                autoplayHoverPause: true,
-                dots: dots_val,
-                nav: nav_val,
-                navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: false
-                    },
-                    600: {
-                        items: 1,
-                        nav: false
-                    },
-                    1000: {
-                        items: items_val
-                    }
-                }
-            });
-        });
-    }
-
-    //LATEST NEWS
-    if ($("#latest_news").length) {
-        $("#latest_news").owlCarousel({
-            items: 3,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: true
-                },
-                600: {
-                    items: 2,
-                    nav: false
-                },
-                1000: {
-                    items: 2,
-                    nav: true,
-                    loop: true
-                }
-            },
-            nav: true,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-        });
-    }
-
-    // GOOGLE MAP FOR CONTACT & EVENT PAGE.
-    if ($('#map_canvas').length) {
-        var map;
-        $('#map_canvas').css({
-            'height': '400px'
-        });
-
-        map = new GMaps({
-            div: '#map_canvas',
-            lat: -12.043333,
-            lng: -77.028333
-        });
-
-        map.addMarker({
-            lat: -12.043333,
-            lng: -77.028333,
-            title: 'Lima',
-            click: function (e) {
-                alert('You clicked in this marker');
-            }
-        });
-    }
-
-    // VENOBOX VIDEO.
-    $(document).ready(function () {
-        $('.venobox').venobox();
-    });
-
-    // COUNTDOWN.
-    if ($('#coming_countdown').length > 0) {
-        $('#coming_countdown').countdown('2020/10/10', function (event) {
-            var $this = $(this).html(event.strftime(''
-                + '<div class="countdown_container"><span class="count_number">%d</span><span class="count_text">Days</span></div>'
-                + '<div class="countdown_container"><span class="count_number">%H</span><span class="count_text">Hours</span></div>'
-                + '<div class="countdown_container"><span class="count_number">%M</span><span class="count_text">Miniutes</span></div>'
-                + '<div class="countdown_container"><span class="count_number">%S</span><span class="count_text">Seconds</span></div>'));
-        });
-    }
-
-
     // CONTACT FORM 
     function email_checkRegexp(o, regexp) {
         if (!(regexp.test(o.val()))) {
@@ -897,10 +568,6 @@ jQuery(function ($) {
 
     //WoW Animation.
     animations();
-
-    //One Page Scrolling.
-    //    smooth_scrolling();
-
 
     // BACK TO TOP BUTTON.
     if ($('#backTop').length === 1) {
@@ -930,11 +597,3 @@ jQuery(function ($) {
         bwl_on_load_scroll();
     });
 });
-
-
-
-
-
-
-
-
